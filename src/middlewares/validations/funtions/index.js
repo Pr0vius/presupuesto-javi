c391e9a88cb82b1a4d9c38fd574521229222a4c3
Braian Delgado <braiandelgado4255@hotmail.com>
2021-12-22 14:31:08 -0300

    validateJWT function added

--- src/middlewares/validations/funtions/index.js
+++ src/middlewares/validations/funtions/index.js
@@ -1,11 +1,30 @@
 const { validationResult } = require("express-validator");
 const ErrorResponse = require("../../../helpers/ErrorResponse");
 const userRepo = require("../../../repository/User");
+const AuthService = require("../../../service/auth");
 
 const emailIsUniqueValidator = async email => {
   const userFound = await userRepo.findByEmail(email);
   if (userFound) {
     throw new ErrorResponse(400, undefined, "Username already exist");
+  }
+};
+
+const validateJWT = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization");
+    const user = await AuthService.validateToken(token);
+    req.user = user;
+    next();
+  } catch (err) {
+    next(
+      new ErrorResponse(
+        err.status || 400,
+        err.message || "Can't validate the token",
+        err.data ||
+          "Please provide an authentification token in Authorization header"
+      )
+    );
   }
 };
 
@@ -23,5 +42,6 @@ const validResult = (req, res, next) => {
 
 module.export = {
   emailIsUniqueValidator,
+  validateJWT,
   validResult,
 };
